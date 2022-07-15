b6cdc9d047f766d5601aa194efac1b5ef6db3b4d
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-15 09:26:52 -0400

    Updating demos

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ function square(number) {
   console.log(Math.pow(number, 2));
 }
 
+// Inner path
 csvTransform({
   path: "./demo.csv",
+  onRead: square,
+  beforeRead: () => console.log("Start reading"),
+  afterRead: () => console.log("After reading"),
+});
+
+// From url
+csvTransform({
+  path: "https://raw.githubusercontent.com/alan-serrano/csv_demo/master/demo.csv",
   onRead: square,
   beforeRead: () => console.log("Start reading"),
   afterRead: () => console.log("After reading"),
