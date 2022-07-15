585723ee54f1757499220e9968ed2f817f34444b
alan-serrano <alan.serrano.pch@gmail.com>
2022-07-15 10:06:44 -0400

    Updating demos

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ function square(number) {
   console.log(Math.pow(number, 2));
 }
 
-// Inner path
-csvTransform({
-  path: "./demo.csv",
-  onRead: square,
-  beforeRead: () => console.log("Start reading"),
-  afterRead: () => console.log("After reading"),
-});
+(async () => {
+  let result;
+  // Inner path
+  result = await csvTransform({
+    path: "./demo.csv",
+    onRead: square,
+    beforeRead: ({ path }) => console.log(`before reading ${path}`),
+    afterRead: () => console.log("After reading"),
+  });
 
-// From url
-csvTransform({
-  path: "https://raw.githubusercontent.com/alan-serrano/csv_demo/master/demo.csv",
-  onRead: square,
-  beforeRead: () => console.log("Start reading"),
-  afterRead: () => console.log("After reading"),
-});
+  console.log("csv", result);
+
+  // From url
+  result = await csvTransform({
+    path: "https://raw.githubusercontent.com/alan-serrano/csv_demo/master/demo.csv",
+    onRead: square,
+    beforeRead: ({ path }) => console.log(`before reading ${path}`),
+    afterRead: () => console.log("After reading"),
+  });
+
+  console.log("csv", result);
+})();
